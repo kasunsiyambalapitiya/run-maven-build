088904f40881d08a352f560beea6d523a36da311
kasunsiyambalapitiya <kasun.siyambalapitiya@gmail.com>
2017-11-08 19:49:41 +0530

    Remove use of maven dependency plugin

--- update-c5-products.go
+++ update-c5-products.go
@@ -6,14 +6,76 @@ import (
 	"io"
 	"os/exec"
 	"fmt"
+	"archive/zip"
+	"path/filepath"
 )
 
 func main() {
+	// Download the pom from server
 	downloadPOM("http://10.100.1.85:8484/pom.xml", "pom.xml")
+	// Extract the given distribution
+	extractDistribution("./c5-custom-product-5.3.0.zip", "./")
+	// Update the distribution
 	err := updateDistribution()
 	if err != nil {
 		fmt.Print(err)
 	}
+
+}
+
+// Extracts the given distribution
+func extractDistribution(distributionLocation, destination string) error {
+	zipReader, err := zip.OpenReader(distributionLocation)
+	if err != nil {
+		return err
+	}
+	defer zipReader.Close()
+
+	// Extract each file to the given destination
+	for _, file := range zipReader.File {
+		extractFile(file, destination)
+	}
+
+	return nil
+}
+
+// Extracts each file to the given distribution to the given location
+func extractFile(file *zip.File, destination string) error {
+	fileContent, error := file.Open()
+	if error != nil {
+		return error
+	}
+	defer fileContent.Close()
+
+	// Create the file path
+	path := filepath.Join(destination, file.Name)
+
+	if file.FileInfo().IsDir() {
+		err := os.MkdirAll(path, 0777)
+		if err != nil {
+			return err
+		}
+	} else {
+		// Create parent directories if any
+		err := os.MkdirAll(filepath.Dir(path), 0777)
+		if err != nil {
+			return err
+		}
+		// Open the file for writing
+		openedFile, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0777)
+		if err != nil {
+			return err
+		}
+
+		// Write the content to the opened file
+		_, err = io.Copy(openedFile, fileContent)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Todo change the m2 location
+	return nil
 }
 
 // Downloads the pom from server
